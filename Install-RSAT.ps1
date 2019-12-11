cf2e7e870167d1ef74ec170f4ea73231390c66e8
Piotr Stremlau <p_stremlau@wargaming.net>
2019-12-11 10:02:19 +0100

    recent updates

--- Install-RSAT.ps1
+++ Install-RSAT.ps1
@@ -9,3 +9,64 @@ Restart-Service wuauserv
 
 # Command to test if everything is installed: 
 Get-WindowsCapability -Name RSAT* -Online | Select-Object -Property DisplayName, State 
+
+
+# Main Loop below
+Get-WindowsCapability -name RSAT* -Online | ForEach-Object {
+    If($_ -eq "Installed"){
+        Write-Output "True!!!!"
+    }
+    else {
+        Write-Output "FALSE"
+    }
+}
+
+
+if ((Get-WindowsCapability -name RSAT* -Online).State -contains "NotPresent") {
+    Write-Output (Get-WindowsCapability -name RSAT* -Online).State
+    Write-Output "Something is Missing..."
+    Get-WindowsCapability -Name RSAT* -Online | Add-WindowsCapability -Online
+}
+else {
+    Write-Output "OK!"
+}
+
+
+$check = Get-WindowsCapability -name RSAT* -Online
+
+foreach ($state in $check.State) {
+    If ($state -eq "NotPresent"){
+        Write-Output "It's Not Present"
+        Write-Output "Code would execute here...."
+        Write-Output $state
+    }
+    else {
+        Write-Output "This one is OK"
+    }
+}
+
+# Check status of RSAT packages installed and store them in an array (?)
+$check = Get-WindowsCapability -name RSAT* -Online
+
+# Loop through each individual object in the array
+foreach ($value in $check) {
+    # Check if object state is NotPresent, if it is, proceed with it's installation
+    If ($value.State -eq "NotPresent"){
+        # This block finds that something is not installs and then move to try to install it.
+        #Line below is just debug
+        Write-Output $value
+        try {
+            #Try to install it
+            Add-WindowsCapability -Name $value.Name –Online -ErrorAction Stop
+        }
+        catch {
+            $ErrorMessage = $_.Exception
+            Write-Output $ErrorMessage
+        }
+    }
+    else {
+        # This block says everything is OK
+        #Line below is just debug
+        Write-Output "This one is OK"
+    }
+}
