4ba47cfa5f2b8b8798c68bd9a500789ef965e1a6
TimekillerTK <erwartungen@protonmail.com>
2020-09-08 08:42:41 +0200

    fixed try/catch blocks, need to work on cleanup at the end of the script

--- Install-RSAT.ps1
+++ Install-RSAT.ps1
@@ -1,7 +1,7 @@
 # Should also create a check to validate powershell version as 7+ and to validate running as admin.
 
 # Function for the building block of objects which will store information about current registry status
-function Store-RegistryValue ($Name, $Path, $PropertyType, $Value) {
+function Store-RegistryValue ($Name, $Path, $PropertyType, $Value, $Exists, $Changed) {
 
     # properties of the future objects
     $properties = @{
@@ -10,6 +10,8 @@ function Store-RegistryValue ($Name, $Path, $PropertyType, $Value) {
         Path = $Path
         PropertyType = $PropertyType
         Value = $Value
+        Exists = [bool]$Exists
+        Changed = [bool]$Changed
 
     }
 
@@ -36,49 +38,49 @@ $savedWU = $false
 #     $savedRCSS = Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Servicing" -Name "RepairContentServerSource" | Select-Object -ExpandProperty RepairContentServerSource
 # }
 
-# Set default values to $null 
-$UseWUServer = $null
-$LocalSourcePath = $null
-$RepairContentServerSource = $null
 
-# Check whether the items exist in Try/Catch blocks 
+# Check whether the items exist in Try/Catch blocks.
+# These blocks are objectively bad, rewrite later in a single function that is called for the different registry properties.
 try {
     # ErrorAction Stop is necessary for the try/catch blocks to catch the error since it's a non-terminating error
     $checkUseWUServer = Get-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" -Name "UseWUServer" -ErrorAction Stop    
-    $UseWUServer = Store-RegistryValue -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" -Name "UseWUServer" -Value $($checkUseWUServer.UseWUServer) -PropertyType DWord
-
+    
+    # Runs only if there's no error
+    $key = [Microsoft.Win32.Registry]::LocalMachine.OpenSubKey('SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU')
+    $UseWUServer = Store-RegistryValue -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" -Name "UseWUServer" -Value $($checkUseWUServer.UseWUServer) -PropertyType $key.GetValueKind('UseWUServer') -Exists $true -Changed $false
+    $key.Dispose()
 }
 catch {
-    $UseWUServer = "notexist"
+    $UseWUServer = Store-RegistryValue -Name UseWUServer -Exists $false -Changed $false
 }
 
 try {
     # ErrorAction Stop is necessary for the try/catch blocks to catch the error since it's a non-terminating error
-    $checkLocalSourcePath = Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Servicing" -Name "LocalSourcePath" -ErrorAction Stop
-    $LocalSourcePath = Store-RegistryValue -Name LocalSourcePath -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Servicing" -PropertyType DWord -Value $null
-    
+    $checkLocalSourcePath = Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Servicing" -Name "LocalSourcePath" -ErrorAction Stop    
+
+    # Runs only if there's no error
+    $key = [Microsoft.Win32.Registry]::LocalMachine.OpenSubKey('SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Servicing')
+    $LocalSourcePath = Store-RegistryValue -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Servicing" -Name "LocalSourcePath" -Value $($checkLocalSourcePath.LocalSourcePath) -PropertyType $key.GetValueKind('LocalSourcePath') -Exists $true -Changed $false
+    $key.Dispose()
 }
 catch {
-    $LocalSourcePath = "notexist"
+    $LocalSourcePath = Store-RegistryValue -Name LocalSourcePath -Exists $false -Changed $false
 }
 
 try {
+
     # ErrorAction Stop is necessary for the try/catch blocks to catch the error since it's a non-terminating error
-    $checkRepairContentServerSource = Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Servicing" -Name "RepairContentServerSource" -ErrorAction Stop
-    $RepairContentServerSource = Store-RegistryValue -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Servicing" -PropertyType DWord -Name RepairContentServerSource -Value 2
-    
+    $checkRepairContentServerSource = Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Servicing" -Name "RepairContentServerSource" -ErrorAction Stop    
+    # Runs only if there's no error
+    $key = [Microsoft.Win32.Registry]::LocalMachine.OpenSubKey('SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Servicing')
+    $RepairContentServerSource = Store-RegistryValue -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Servicing" -Name "RepairContentServerSource" -Value $($checkRepairContentServerSource.RepairContentServerSource) -PropertyType $key.GetValueKind('RepairContentServerSource')  -Exists $true  -Changed $false
+    $key.Dispose()
+
 }
 catch {
-    $RepairContentServerSource = "notexist"
+    $RepairContentServerSource = Store-RegistryValue -Name RepairContentServerSource -Exists $false -Changed $false
 }
 
-
-
-
-if ($RepairContentServerSource) {
-    $UseWUServer = Store-RegistryValue -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" -Name "UseWUServer" -Value $($checkUseWUServer.UseWUServer) -PropertyType DWord
-    #$savedWU = Get-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" -Name "UseWUServer" | Select-Object -ExpandProperty UseWUServer
-}
 
 
 # Check status of RSAT packages installed in a variable
@@ -86,11 +88,6 @@ $check = Get-WindowsCapability -name RSAT* -Online
 
 # Define $finishloop being false
 $finishloop = $false
-
-# Memorize current values of registry keys that will be modified (To be added later)
-# $currentWU = Get-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" -Name "UseWUServer" | select -ExpandProperty UseWUServer
-# $currentLSP = Get-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Servicing -Name LocalSourcePath
-# $currentRCSS = Get-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Servicing -Name RepairContentServerSource
 
 # Loop through each individual object in the array
 foreach ($value in $check) {
@@ -136,6 +133,11 @@ foreach ($value in $check) {
                     New-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Servicing -PropertyType ExpandString -Name LocalSourcePath
                     New-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Servicing -PropertyType DWord -Name RepairContentServerSource -Value 2
 
+                    # flagging changes
+                    $LocalSourcePath.Changed = $true
+                    $RepairContentServerSource.Changed = $true
+
+
                 }
                 elseif ($ErrorMessage -like "*0x8024002e*") {
                     # In case of error 0x8024002e, perform the following tasks to fix it
@@ -146,6 +148,9 @@ foreach ($value in $check) {
                     # }
 
                     Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" -Name "UseWUServer" -Value 0
+
+                    # flagging changes
+                    $UseWUServer.Changed = $true
 
                     Write-Host "Restarting service wuauserv"
                     Restart-Service wuauserv
@@ -164,30 +169,14 @@ foreach ($value in $check) {
     }
 }
 
-
-
 # Revert the changes made during script run
+# Cleanup happens here
 
-# LSP: Test current value, if different from one set at start, revert.
-# RCSS: Test current value, if different from one set at start, revert.
-# WU: Test current value, if different from one set at start, revert.
-
-$currentLSP = 
-
-if ($savedLSP -ne $currentLSP) {
-    Write-Output "Value different. No action"
+$LocalSourcePath, $RepairContentServerSource, $UseWUServer | ForEach-Object {
+    Write-Host "Reverting changes made to registry..."
+    if ($_.Changed -eq $true) {
+        Write-Host "Currently fixing $($_.Name)" -ForegroundColor Magenta
+    } else {
+        Write-Host "No change to $($_.Name)..." -ForegroundColor Cyan
+    }
 }
-else {
-
-}
-$savedRCSS
-$savedWU
-
-If ($(Store-RegistryValue -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Servicing" -Value LocalSourcePath) -eq $false) {
-
-}
-
-Store-RegistryValue -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Servicing" -Value RepairContentServerSource
-Store-RegistryValue -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" -Value UseWUServer
-
-Remove-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Servicing -Name LocalSourcePath
